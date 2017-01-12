1e043356402423ccef4b2d6925e9b55f660a5b44
Zachary Kirby <zkirby16@gmail.com>
2017-01-11 21:44:24 -0800

    Updated Nav buttons
    Added functionality for the nav buttons so the page changes when they are pressed

---
+++ JavaScript/subpage.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+	// Button object with all the button info
+	var buttons = { "nav-button button-one": 
+							{"object": $(".nav-button.button-one"), 
+							 "body": $(".me-body"),
+							 "group": "1"},
+					"nav-button button-two":
+							{"object": $(".nav-button.button-two"), 
+							 "body": $(".organizations-body"),
+							 "group": "1"},
+					"nav-button button-three":
+							{"object": $(".nav-button.button-three"), 
+							 "body": $(".contact-body"),
+							 "group": "1"},
+					};
+
+	// Set the first buttons active
+	set_active(buttons["nav-button button-one"]["object"]);
+	var last_active = buttons["nav-button button-one"];
+
+	$("div[class~=nav-button]").click(function(){
+		var button = buttons[this.className];
+
+		if(button != last_active){
+			if(button["group"] == last_active["group"]){
+				switch_showing(button, last_active);
+			    set_active(button["object"]); 
+			    set_unactive(last_active["object"]);
+			    last_active = button;
+			}
+		};
+	});
+
+});
+
+function switch_showing(curr, prev){
+	curr["body"].css("display", "block");
+	prev["body"].css("display", "none");
+};
+
+function set_active(obj){
+	obj.css("background", "#13293D");
+}
+
+function set_unactive(obj){
+	obj.removeAttr("style");
+}
